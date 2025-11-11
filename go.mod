93d6079b917ed524cb24e634e4a15f2235548574
VJ-2303 <vanaraj1018@gmail.com>
2025-11-11 19:08:45 +0530

    Written tokens Models for Creating new Auth Tokens

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/VJ-2303/ecommerce-api-go
 go 1.25.1
 
 require (
+	github.com/golang-jwt/jwt/v5 v5.3.0
 	github.com/julienschmidt/httprouter v1.3.0
 	github.com/lib/pq v1.10.9
 	golang.org/x/crypto v0.43.0
---
+++ internal/data/tokens.go
@@ -0,0 +1,42 @@
+package data
+
+import (
+	"database/sql"
+	"fmt"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+)
+
+type Token struct {
+	PlainText string    `json:"token"`
+	UserID    int64     `json:"-"`
+	Expiry    time.Time `json:"expiry"`
+	Scope     string    `json:"-"`
+}
+
+type TokenModel struct {
+	DB *sql.DB
+}
+
+func (m TokenModel) New(userID int64, expiry time.Duration, Scope, secretKey string) (*Token, error) {
+	token := &Token{
+		UserID: userID,
+		Expiry: time.Now().Add(expiry),
+		Scope:  Scope,
+	}
+	claims := jwt.MapClaims{}
+	claims["exp"] = token.Expiry.Unix()
+	claims["iat"] = time.Now()
+	claims["scope"] = Scope
+	claims["sub"] = fmt.Sprintf("%d", userID)
+
+	jwtString := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+	signedString, err := jwtString.SignedString(secretKey)
+	if err != nil {
+		return nil, err
+	}
+	token.PlainText = signedString
+	return token, nil
+}
